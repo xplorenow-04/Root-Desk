9ebf90c06c2454abeffdaf11dabe29baa02b54cc
Atish Thorat <atishthorat2004@gmail.com>
2026-07-23 14:22:50 +0530

    first commit v1

--- client/vite.config.js
+++ client/vite.config.js
@@ -14,7 +14,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://root-desk.onrender.com',
         changeOrigin: true,
       },
     },
